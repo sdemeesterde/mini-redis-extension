64730f53fb607d43ac2717f822af193afd53aeea
sdemeesterde <samuel.dem22@gmail.com>
2026-04-06 13:03:35 +0200

    feat: start implementing buffer client for all commands

--- src/clients/buffered_client.rs
+++ src/clients/buffered_client.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::clients::Client;
 use crate::Result;
 
@@ -8,8 +10,40 @@ use tokio::sync::oneshot;
 // Enum used to message pass the requested command from the `BufferedClient` handle
 #[derive(Debug)]
 enum Command {
+    // key
     Get(String),
-    Set(String, Bytes),
+    // key, value, expire
+    Set(String, Bytes, Option<Duration>),
+    // keys
+    Del(Vec<String>),
+    // key, members
+    Sadd(String, Vec<String>),
+    // key, member
+    Sismember(String, String),
+    // key
+    Slength(String),
+    // key, members
+    Srem(String, Vec<String>),
+    // key, (score, member)
+    Zadd(String, Vec<(u64, String)>),
+    // key, start, stop, rev, offset, count
+    Zrange(String, u64, u64, bool, Option<u64>, Option<u64>),
+    // key, members
+    Zrem(String, Vec<String>),
+}
+
+#[derive(Debug)]
+enum Response {
+    Get(Result<Option<Bytes>>),
+    Set(Result<()>),
+    Del(Result<usize>),
+    Sadd(Result<usize>),
+    Sismember(Result<usize>),
+    Slength(Result<usize>),
+    Srem(Result<usize>),
+    Zadd(Result<usize>),
+    Zrange(Result<Vec<(u64, String)>>),
+    Zrem(Result<usize>),
 }
 
 // Message type sent over the channel to the connection task.
@@ -19,7 +53,7 @@ enum Command {
 // `oneshot::Sender` is a channel type that sends a **single** value. It is used
 // here to send the response received from the connection back to the original
 // requester.
-type Message = (Command, oneshot::Sender<Result<Option<Bytes>>>);
+type Message = (Command, oneshot::Sender<Response>);
 
 /// Receive commands sent through the channel and forward them to client. The
 /// response is returned back to the caller via a `oneshot`.
@@ -30,8 +64,26 @@ async fn run(mut client: Client, mut rx: Receiver<Message>) {
     while let Some((cmd, tx)) = rx.recv().await {
         // The command is forwarded to the connection
         let response = match cmd {
-            Command::Get(key) => client.get(&key).await,
-            Command::Set(key, value) => client.set(&key, value).await.map(|_| None),
+            Command::Get(key) => Response::Get(client.get(&key).await),
+            Command::Set(key, value, expiration) => {
+                if let Some(d) = expiration {
+                    Response::Set(client.set_expires(&key, value, d).await)
+                } else {
+                    Response::Set(client.set(&key, value).await)
+                }
+            }
+            Command::Del(keys) => Response::Del(client.deletes(&keys).await),
+            Command::Sadd(key, members) => Response::Sadd(client.sadd(&key, members).await),
+            Command::Sismember(key, member) => {
+                Response::Sismember(client.sismember(&key, &member).await)
+            }
+            Command::Slength(key) => Response::Slength(client.slength(&key).await),
+            Command::Srem(key, members) => Response::Srem(client.srem(&key, members).await),
+            Command::Zadd(key, entries) => Response::Zadd(client.zadd(&key, entries).await),
+            Command::Zrange(key, start, stop, rev, offset, count) => {
+                Response::Zrange(client.zrange(&key, start, stop, rev, offset, count).await)
+            }
+            Command::Zrem(key, members) => Response::Zrem(client.zrem(&key, members).await),
         };
 
         // Send the response back to the caller.
@@ -92,7 +144,8 @@ impl BufferedClient {
 
         // Await the response
         match rx.await {
-            Ok(res) => res,
+            Ok(Response::Get(res)) => res,
+            Ok(_) => Err("Wrong response type received for GET.".into()),
             Err(err) => Err(err.into()),
         }
     }
@@ -101,9 +154,14 @@ impl BufferedClient {
     ///
     /// Same as `Client::set` but requests are **buffered** until the associated
     /// connection has the ability to send the request
-    pub async fn set(&mut self, key: &str, value: Bytes) -> Result<()> {
+    pub async fn set(
+        &mut self,
+        key: &str,
+        value: Bytes,
+        expiration: Option<Duration>,
+    ) -> Result<()> {
         // Initialize a new `Set` command to send via the channel.
-        let set = Command::Set(key.into(), value);
+        let set = Command::Set(key.into(), value, expiration);
 
         // Initialize a new oneshot to be used to receive the response back from the connection.
         let (tx, rx) = oneshot::channel();
@@ -113,7 +171,8 @@ impl BufferedClient {
 
         // Await the response
         match rx.await {
-            Ok(res) => res.map(|_| ()),
+            Ok(Response::Set(res)) => res,
+            Ok(_) => Err("Wrong response type received for SET.".into()),
             Err(err) => Err(err.into()),
         }
     }
